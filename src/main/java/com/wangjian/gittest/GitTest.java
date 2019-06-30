c4ebbf8d13584743a8c2fff12c3ad32d27bd9c65
wj <wjkuhao@163.com>
2019-06-30 22:09:36 +0800

    创建的新项目

---
+++ src/main/java/com/wangjian/gittest/GitTest.java
@@ -0,0 +1,15 @@
+package com.wangjian.gittest;
+
+/**
+ * @author Wj
+ * @date 2019/06/30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好啊，我是git");
+        System.out.println("你好啊，我是git");
+        System.out.println("你好啊，我是git");
+        System.out.println("你好啊，我是git");
+        System.out.println("你好啊，我是git");
+    }
+}
